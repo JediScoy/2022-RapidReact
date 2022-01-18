f98e625660dcbeab965671760684835b07696852
Jestin VanScoyoc <coyotepod@gmail.com>
2022-01-18 08:18:08 -0500

    New, generic subsystems

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,47 @@
+
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+// import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+// import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+// import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+// Shuffleboard imports
+// import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts; // Displaying data?
+// import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard; // Displaying data?
+// import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab; // Displaying data?
+
+// CTRE imports
+// import com.ctre.phoenix.motorcontrol.ControlMode;
+// import com.ctre.phoenix.motorcontrol.NeutralMode;
+// import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+// import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+// import com.ctre.phoenix.motorcontrol.can.TalonFX;
+// import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+// import com.ctre.phoenix.motorcontrol.TalonFXSensorCollection;
+
+public class IntakeSubsystem extends SubsystemBase {
+/** Creates a Subsystem using Falcon 500s controlled by TalonFX.
+    * These are the two motors for [ ]
+**/
+
+  /** Uncomment setup for each specific subystem with motors
+  private final MotorController m_IntakeMotors = 
+    /** new MotorControllerGroup(
+        * new WPI_TalonFX(LauncherConstants.leftLaunchMotor),
+        * new WPI_TalonFX(LauncherConstants.rightLaunchMotor));
+  */
+    
+// final TalonFXInvertType rightLaunchMotor = TalonFXInvertType.CounterClockwise;
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+
+} // End of Class
