8b2a8ccd55dc20b101ebcee0d6cadab6da9aae0e
Jestin VanScoyoc <coyotepod@gmail.com>
2022-01-21 10:48:43 -0500

    "Voiding" the subsystems for now to avoid code errors
    Mostly changing .java to .txt files of commands and subsystems

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import static edu.wpi.first.wpilibj.XboxController.Button;
+// import static edu.wpi.first.wpilibj.XboxController.Button;
 // import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 // import edu.wpi.first.wpilibj2.command.FunctionalCommand;
@@ -14,11 +14,12 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 // import edu.wpi.first.wpilibj2.command.button.Button;
 import frc.robot.commands.DefaultDriveCommand;
 import frc.robot.subsystems.DrivetrainSubsystem;
-import frc.robot.commands.LaunchCargoLow;
-import frc.robot.commands.LaunchCargoHigh;
-import frc.robot.commands.StopLaunch;
-import frc.robot.subsystems.LauncherSubsystem;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+// import frc.robot.commands.LaunchCargoLow;
+// import frc.robot.commands.LaunchCargoHigh;
+// import frc.robot.commands.StopLaunch;
+// import frc.robot.subsystems.LauncherSubsystem;
+
+// import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 
 /**
@@ -33,9 +34,10 @@ public class RobotContainer {
 
   private final XboxController m_controller = new XboxController(0);
 
-  private final LauncherSubsystem m_launcherSubsystem = new LauncherSubsystem();
+  // private final LauncherSubsystem m_launcherSubsystem = new LauncherSubsystem();
+
   // Robot Commands
-  private final LaunchCargoLow m_autoCommand = new LaunchCargoLow(m_launcherSubsystem);
+  // private final LaunchCargoLow m_autoCommand = new LaunchCargoLow(m_launcherSubsystem);
 
   // private final XboxController m_joystick = new XboxController(0);
 
@@ -60,7 +62,6 @@ public class RobotContainer {
     // Configure the button bindings
     configureButtonBindings();
   }
-
   /**
    * Use this method to define your button->command mappings. Buttons can be created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
@@ -70,31 +71,33 @@ public class RobotContainer {
   private void configureButtonBindings() {
     //declaring buttons on controller
       
+  /** COMMENTING OUT LAUNCHER CODE FOR PRACTICE BOT 
     final JoystickButton r1 = new JoystickButton(m_controller, Button.kRightBumper.value);
     final JoystickButton l1 = new JoystickButton(m_controller, Button.kLeftBumper.value);
 
     // Connect the buttons to commands
     // Launch the Cargo when either left bumper or right bumper is held
     // left bumper = low shot, right bumper = high shot
-    /* We tried whileHeld command initially, but it only starts the motors, it does not stop the motors automatically
-    upon button release as it should */
+    // We tried whileHeld command initially, but it only starts the motors, it does not stop the motors automatically
+    upon button release as it should
     //this is working to start falcon motors when button held
     l1.whenHeld(new LaunchCargoLow(m_launcherSubsystem));
     r1.whenHeld(new LaunchCargoHigh(m_launcherSubsystem));
     //added a when button released command until we have whileHeld working as it should
     l1.whenReleased(new StopLaunch(m_launcherSubsystem));
     r1.whenReleased(new StopLaunch(m_launcherSubsystem));
-
+    **/
 
   /** Use this to pass the autonomous command to the main {@link Robot} class.
   * @return the command to run in autonomous
  */
-    // Back button zeros the gyroscope
-    /** Shaun's previously working code (?) that we broke :) 
-    new Button(m_controller::getBackButton)
+  
+  // Back button zeros the gyroscope
+  // Shaun's previously working code (?) that we broke :) 
+  /** new Button(m_controller::getBackButton)
             // No requirements because we don't need to interrupt anything
             .whenPressed(m_drivetrainSubsystem::zeroGyroscope);
-    */
+    */    
 
   }
 
