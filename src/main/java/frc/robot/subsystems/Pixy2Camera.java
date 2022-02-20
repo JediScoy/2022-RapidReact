ffadcc1087707979243dbf8d4635d34e1ce63f44
Jestin VanScoyoc <coyotepod@gmail.com>
2022-02-20 15:01:17 -0500

    pixy2 implementation
    just getting started

---
+++ src/main/java/frc/robot/subsystems/Pixy2Camera.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.ArrayList;
+
+import io.github.pseudoresonance.pixy2api.Pixy2;
+import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
+import io.github.pseudoresonance.pixy2api.links.*;
+
+
+public class Pixy2Camera {
+
+	private static final Pixy2 pixy;
+
+
+
+	public static void initialize() {
+		pixy = Pixy2.createInstance(new SPILink()); // Creates a new Pixy2 camera using SPILink
+		pixy.init(); // Initializes the camera and prepares to send/receive data
+		pixy.setLamp((byte) 1, (byte) 1); // Turns the LEDs on
+		pixy.setLED(200, 30, 255); // Sets the RGB LED to purple
+	}
+  public static Block getBiggestBlock() {
+		// Gets the number of "blocks", identified targets, that match signature 1 on the Pixy2,
+		// does not wait for new data if none is available,
+		// and limits the number of returned blocks to 25, for a slight increase in efficiency
+		int blockCount = pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1, 25);
+		System.out.println("Found " + blockCount + " blocks!"); // Reports number of blocks found
+		if (blockCount <= 0) {
+			return null; // If blocks were not found, stop processing
+		}
+		ArrayList<Block> blocks = pixy.getCCC().getBlockCache(); // Gets a list of all blocks found by the Pixy2
+		Block largestBlock = null;
+		for (Block block : blocks) { // Loops through all blocks and finds the widest one
+			if (largestBlock == null) {
+				largestBlock = block;
+			} else if (block.getWidth() > largestBlock.getWidth()) {
+				largestBlock = block;
+			}
+		}
+		return largestBlock;
+	}
+} //End of class
