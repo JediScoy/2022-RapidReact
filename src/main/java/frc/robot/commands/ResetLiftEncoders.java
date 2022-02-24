d3fec15e6d4144d02b06294c235046f1e3535d6b
Chad Marshall <cdmarshall25@gmail.com>
2022-02-24 09:42:09 -0500

    added call for resetEncoders to initilize

--- src/main/java/frc/robot/commands/ResetLiftEncoders.java
+++ src/main/java/frc/robot/commands/ResetLiftEncoders.java
@@ -5,16 +5,25 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Lift;
 
 public class ResetLiftEncoders extends CommandBase {
+  private Lift subsystem;
+
   /** Creates a new ResetLiftEncoders. */
   public ResetLiftEncoders() {
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+
+    //trying to call resetEncoders for Lift motors on Robot startup?
+      subsystem.resetEncoders();
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
