f5cecbb5e2b2d5610a2ae0dd7485ecb7f49b47cf
Shaun <Shaunfruggie1257@gmail.com>
2022-02-28 18:44:49 -0500

    made a slight modification to isfinished

--- src/main/java/frc/robot/commands/auton/Red3.java
+++ src/main/java/frc/robot/commands/auton/Red3.java
@@ -19,6 +19,7 @@ import frc.robot.Constants;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
 public class Red3 extends CommandBase{
+        boolean isFin = false;
     TrajectoryConfig trajectoryConfig;
     Trajectory trajectory;
     PIDController xController;
@@ -35,9 +36,9 @@ public Red3(DrivetrainSubsystem m_drivetrainSubsystem) {
         trajectory = TrajectoryGenerator.generateTrajectory(
             new Pose2d(0, 0, new Rotation2d(0)),
             List.of(
-                    new Translation2d(1, 0),
-                    new Translation2d(1, -1)),
-            new Pose2d(2, -1, Rotation2d.fromDegrees(180)),
+                    new Translation2d(5, 0),
+                    new Translation2d(5, 5)),
+            new Pose2d(10, 5, Rotation2d.fromDegrees(360)), //full rotation?
             trajectoryConfig
             );
     
@@ -66,8 +67,18 @@ public Red3(DrivetrainSubsystem m_drivetrainSubsystem) {
                    m_drivetrainSubsystem.resetOdometry(trajectory.getInitialPose());
                  swerveControllerCommand.execute();
                     m_drivetrainSubsystem.stop();
+                    isFin = true;
         
 
             }
    
+            @Override
+            public boolean isFinished() {
+                    if (isFin) {
+                          return true  
+                    }
+                    else{
+                            return false;
+                    }
+            }
 }
